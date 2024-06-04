b4103ad6968c888a6d77eb8bf9511064496108a2
Adisdzaky <adisdzakyrivai@gmail.com>
2024-06-04 13:56:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 global.creator = 'RelixOfficial'// yourname
-global.MONGO_DB_URI = "mongodb+srv://relix:relix@cluster0.4wmilcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://relixx:relixx@cluster0.enc0ohu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "relixofficial4@gmail.com" //email
 global.email_password = "ypxutisexyvqgmqb" //application password email
